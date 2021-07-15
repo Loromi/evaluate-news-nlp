5306dc08d5533334648b31ac0d3018e2bce9b040
Romi <>
2021-07-15 10:58:09 +0200

    feat: added key to explain sentiment score

--- src/client/index.js
+++ src/client/index.js
@@ -5,6 +5,7 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
+import './styles/results.scss'
 
 fetch('http://localhost:8080/return_data', {
     method: "POST",
